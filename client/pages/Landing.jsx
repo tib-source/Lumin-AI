abb60f38c8075dbcffa1e3967664b0a284c81431
Tibebe Demissie <tib.demissie@gmail.com>
2023-05-10 01:06:40 +0100

    styled landing page

--- client/pages/Landing.jsx
+++ client/pages/Landing.jsx
@@ -1,8 +1,11 @@
 function Landing() {
 	return (
 		<div className="landing">
-			<h1>Lumin</h1>
-			<p>Illuminating minds with the knowledge of AI</p>
+			<div className="landing-main">
+				<h1>Lumin</h1>
+				<p>Illuminating minds with the knowledge of AI</p>
+				<a href="/login">Open Your Mind</a>
+			</div>
 		</div>
 	);
 }
