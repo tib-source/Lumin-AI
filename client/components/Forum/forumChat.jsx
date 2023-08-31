4c92685cf433c257f0e5ef20630f0dbdd3c5aeec
Tsquare25 <tibebe1234t@gmail.com>
2023-08-31 14:21:49 +0100

    ChatSingle import fix

--- client/components/Forum/forumChat.jsx
+++ client/components/Forum/forumChat.jsx
@@ -1,41 +1,41 @@
 import { useEffect, useRef, useState } from "react";
-import ChatSingle from "./chatSingle";
+import ChatSingle from "./ChatSingle";
 import TextInput from "./TextInput";
 
 export default function ForumChat() {
-	const prevData = [
-		"Lorem ipsum dolor, sit amet cosectetur adipisicing elit. Rem, consequatur, Lorem ipsum dolor, sit amet cosectetur adipisicing elit. Rem, consequatur adsfasdf asdfas df asdfasdf ",
-		"Lorem ipsum consequatur",
-		"Lorem ipsum dolor, sit amet  adipisicing elit. Rem, consequatur",
-		"Whats up Fellas",
-	];
+  const prevData = [
+    "Lorem ipsum dolor, sit amet cosectetur adipisicing elit. Rem, consequatur, Lorem ipsum dolor, sit amet cosectetur adipisicing elit. Rem, consequatur adsfasdf asdfas df asdfasdf ",
+    "Lorem ipsum consequatur",
+    "Lorem ipsum dolor, sit amet  adipisicing elit. Rem, consequatur",
+    "Whats up Fellas",
+  ];
 
-	const [data, setData] = useState([]);
-	const scrollRef = useRef(null);
-	useEffect(() => {
-		scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-	}, [data]);
+  const [data, setData] = useState([]);
+  const scrollRef = useRef(null);
+  useEffect(() => {
+    scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+  }, [data]);
 
-	function addData(newData) {
-		setData([...data, newData]);
-		// TODO: fix scrolling issue.
-	}
-	return (
-		<div className="forum__chat">
-			<div className="chat__header">
-				<p>Lorem ipsum dolor sit amet consectetur...</p>
-			</div>
-			<div className="chat__content" id="chat__content" ref={scrollRef}>
-				<ChatSingle textContent={"This is a test, test 123"} />
-				{prevData.map((text, i) => {
-					return <ChatSingle key={i} textContent={text} left={true} />;
-				})}
+  function addData(newData) {
+    setData([...data, newData]);
+    // TODO: fix scrolling issue.
+  }
+  return (
+    <div className="forum__chat">
+      <div className="chat__header">
+        <p>Lorem ipsum dolor sit amet consectetur...</p>
+      </div>
+      <div className="chat__content" id="chat__content" ref={scrollRef}>
+        <ChatSingle textContent={"This is a test, test 123"} />
+        {prevData.map((text, i) => {
+          return <ChatSingle key={i} textContent={text} left={true} />;
+        })}
 
-				{data.map((text, i) => {
-					return <ChatSingle key={i} textContent={text} />;
-				})}
-			</div>
-			<TextInput setData={addData} />
-		</div>
-	);
+        {data.map((text, i) => {
+          return <ChatSingle key={i} textContent={text} />;
+        })}
+      </div>
+      <TextInput setData={addData} />
+    </div>
+  );
 }
